5d1e7f03ce271b35e74fd51822f94e1c08e2e869
Adrian Rangel <adrian.rangel@gmail.com>
2018-04-08 22:19:15 -0500

    add missing new/0

--- lib/islands_engine/board.ex
+++ lib/islands_engine/board.ex
@@ -1,6 +1,8 @@
 defmodule IslandsEngine.Board do
   alias IslandsEngine.{Coordinate, Island}
 
+  def new(), do: %{}
+
   def position_island(board, key, %Island{} = island) do
     case overlaps_existing_island?(board, key, island) do
       true -> {:error, :overlapping_island}
