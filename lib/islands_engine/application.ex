6d6beb819788264d2cc70a4f08428e90fdbc39be
Adrian Rangel <adrian.rangel@gmail.com>
2018-09-21 12:27:33 -0500

    start game supervisor

--- lib/islands_engine/application.ex
+++ lib/islands_engine/application.ex
@@ -8,7 +8,8 @@ defmodule IslandsEngine.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      {Registry, keys: :unique, name: Registry.Game}
+      {Registry, keys: :unique, name: Registry.Game},
+      IslandsEngine.GameSupervisor
       # Starts a worker by calling: IslandsEngine.Worker.start_link(arg)
       # {IslandsEngine.Worker, arg},
     ]
