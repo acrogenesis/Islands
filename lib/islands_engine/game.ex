7c5233a3add1dd88b74ed52dc250de140dac8b5e
Adrian Rangel <adrian.rangel@gmail.com>
2018-09-21 12:02:56 -0500

    add game sup

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,5 +1,5 @@
 defmodule IslandsEngine.Game do
-  use GenServer
+  use GenServer, start: {__MODULE__, :start_link, []}, restart: :transient
   alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
 
   @players [:player1, :player2]
@@ -22,8 +22,7 @@ defmodule IslandsEngine.Game do
   end
 
   def handle_call({:add_player, name}, _from, state_data) do
-    with{:ok, rules} <- Rules.check(state_data.rules, :add_player)
-    do
+    with {:ok, rules} <- Rules.check(state_data.rules, :add_player) do
       state_data
       |> update_player2_name(name)
       |> update_rules(rules)
@@ -35,15 +34,11 @@ defmodule IslandsEngine.Game do
 
   def handle_call({:position_island, player, key, row, col}, _from, state_data) do
     board = player_board(state_data, player)
-    with {:ok, rules} <-
-           Rules.check(state_data.rules, {:position_islands, player}),
-         {:ok, coordinate} <-
-           Coordinate.new(row, col),
-         {:ok, island} <-
-           Island.new(key, coordinate),
-         %{} = board <-
-           Board.position_island(board, key, island)
-    do
+
+    with {:ok, rules} <- Rules.check(state_data.rules, {:position_islands, player}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {:ok, island} <- Island.new(key, coordinate),
+         %{} = board <- Board.position_island(board, key, island) do
       state_data
       |> update_board(player, board)
       |> update_rules(rules)
@@ -51,8 +46,10 @@ defmodule IslandsEngine.Game do
     else
       :error ->
         {:reply, :error, state_data}
+
       {:error, :invalid_coordinate} ->
         {:reply, {:error, :invalid_coordinate}, state_data}
+
       {:error, :invalid_island_type} ->
         {:reply, {:error, :invalid_island_type}, state_data}
     end
@@ -60,9 +57,9 @@ defmodule IslandsEngine.Game do
 
   def handle_call({:set_islands, player}, _from, state_data) do
     board = player_board(state_data, player)
+
     with {:ok, rules} <- Rules.check(state_data.rules, {:set_islands, player}),
-      true <- Board.all_islands_positioned?(board)
-    do
+         true <- Board.all_islands_positioned?(board) do
       state_data
       |> update_rules(rules)
       |> reply_success({:ok, board})
@@ -77,10 +74,10 @@ defmodule IslandsEngine.Game do
     opponent_board = player_board(state_data, opponent_key)
 
     with {:ok, rules} <- Rules.check(state_data.rules, {:guess_coordinate, player_key}),
-      {:ok, coordinate} <- Coordinate.new(row, col),
-      {hit_or_miss, forested_island, win_status, opponent_board} <- Board.guess(opponent_board, coordinate),
-      {:ok, rules} <- Rules.check(rules, {:win_check, win_status})
-    do
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {hit_or_miss, forested_island, win_status, opponent_board} <-
+           Board.guess(opponent_board, coordinate),
+         {:ok, rules} <- Rules.check(rules, {:win_check, win_status}) do
       state_data
       |> update_board(opponent_key, opponent_board)
       |> update_guesses(player_key, hit_or_miss, coordinate)
@@ -128,7 +125,8 @@ defmodule IslandsEngine.Game do
   defp opponent(:player2), do: :player1
 
   defp update_guesses(state_data, player_key, hit_or_miss, coordinate) do
-    update_in(state_data[player_key].guesses, fn guesses -> Guesses.add(guesses, hit_or_miss, coordinate)
+    update_in(state_data[player_key].guesses, fn guesses ->
+      Guesses.add(guesses, hit_or_miss, coordinate)
     end)
   end
 end
